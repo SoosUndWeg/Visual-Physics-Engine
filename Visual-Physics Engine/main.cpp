958c8c2c951829955371dd6b7999c87495e7761a
SoosUndWeg <kilianbrecht@gmx.de>
2025-08-03 08:11:40 +0200

    Initial Commit

---
+++ Visual-Physics Engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Visual-Physics Engine
+//
+//  Created by Kilian Brecht on 03.08.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
